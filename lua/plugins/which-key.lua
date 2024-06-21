2651175b6bec21381d83deb7e0a731991d696c6f
Chris Phillips <phillipsachris@gmail.com>
2024-06-21 13:46:35 -0700

    Add keymaps to which-key plugin

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -12,7 +12,11 @@ return {
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      ['<leader>;'] = { name = '[;]Arrow Buffer', _ = 'which_key_ignore' },
+      ["<leader>'"] = { name = "[']Arrow File', _ = 'which_key_ignore" },
+      ['<leader>u'] = { name = '[U]ndo Tree', _ = 'which_key_ignore' },
+      ['<leader>e'] = { name = '[E]File Explorer', _ = 'which_key_ignore' },
+      ['<leader>*'] = { name = '[*] Git', _ = 'which_key_ignore' },
     }
   end,
 }
-
