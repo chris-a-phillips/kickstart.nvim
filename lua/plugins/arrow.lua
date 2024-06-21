887dd7dfe551bf7a5949f969aa61da9d49303f9e
Chris Phillips <phillipsachris@gmail.com>
2024-06-21 13:46:21 -0700

    Add arrow plugin

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -3,6 +3,18 @@ return {
   opts = {
     show_icons = true,
     leader_key = ';', -- Recommended to be a single key
-    buffer_leader_key = 'm', -- Per Buffer Mappings
+    buffer_leader_key = "'", -- Per Buffer Mappings
+    mappings = {
+      edit = 'e',
+      delete_mode = 'd',
+      clear_all_items = 'C',
+      toggle = 's', -- used as save if separate_save_and_remove is true
+      open_vertical = '|',
+      open_horizontal = '-',
+      quit = 'q',
+      remove = 'x', -- only used if separate_save_and_remove is true
+      next_item = ']',
+      prev_item = '[',
+    },
   },
 }
