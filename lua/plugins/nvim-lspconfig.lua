554aeff7b03e714b6d95ee8de0c4a72a31342e1d
Chris Phillips <phillipsachris@gmail.com>
2024-06-21 13:46:58 -0700

    Improve telescope

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -167,9 +167,10 @@ return {
             completion = {
               callSnippet = 'Replace',
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = {
-            --   globals = { 'vim' },
+            -- {
+            --   diagnostics = {
+            --     globals = { 'vim', 'opts' },
+            --   },
             -- },
           },
         },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,4 @@
--- Fuzzy Finder (files, lsp, etc)
+-- Shmml Svaqre (svyrf, yfc, rgp)
 return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
@@ -112,5 +112,100 @@ return {
 
     -- Custom keymappings
 
+    -- Key mappings for basic Git commands
+    vim.keymap.set('n', '<leader>*a', function()
+      vim.cmd('G add ' .. vim.fn.expand '%')
+    end, { desc = '[*] Git [A]dd current file' })
+    vim.keymap.set('n', '<leader>*C', function()
+      vim.cmd 'G commit'
+    end, { desc = '[*] Git [C]ommit' })
+    vim.keymap.set('n', '<leader>*d', function()
+      vim.cmd 'G diff'
+    end, { desc = '[*] Git [D]iff' })
+    vim.keymap.set('n', '<leader>*s', function()
+      vim.cmd 'G status'
+    end, { desc = '[*] Git [S]tatus' })
+    vim.keymap.set('n', '<leader>*l', function()
+      vim.cmd 'G log'
+    end, { desc = '[*] Git [L]og' })
+    vim.keymap.set('n', '<leader>*p', function()
+      vim.cmd 'G push'
+    end, { desc = '[*] Git [P]ush' })
+    vim.keymap.set('n', '<leader>*u', function()
+      vim.cmd 'G pull'
+    end, { desc = '[*] Git p[U]ll' })
+    vim.keymap.set('n', '<leader>*co', function()
+      vim.cmd 'G checkout'
+    end, { desc = '[*] Git [C]heck[O]ut' })
+    vim.keymap.set('n', '<leader>*x', function()
+      vim.cmd 'G branch'
+    end, { desc = '[*] Git branch' })
+
+    -- Key mappings for advanced Git commands
+    vim.keymap.set('n', '<leader>**rb', function()
+      vim.cmd 'G fetch origin && G rebase origin/master'
+    end, { desc = '[**] Git [R]ebase onto origin/master' })
+    vim.keymap.set('n', '<leader>**crb', function()
+      vim.cmd 'G fetch origin && G checkout --track origin/'
+    end, { desc = '[**] Git [C]heckout [R]emote [B]ranch' })
+    vim.keymap.set('n', '<leader>**sc', function()
+      vim.cmd 'G stash clear'
+    end, { desc = '[**] Git [S]tash [C]lear' })
+    vim.keymap.set('n', '<leader>**sh', function()
+      vim.cmd 'G show'
+    end, { desc = '[**] Git [S]how' })
+    vim.keymap.set('n', '<leader>**uc', function()
+      vim.cmd 'G reset HEAD~1 --mixed'
+    end, { desc = '[**] Git [U]ndo last [C]ommit' })
+    vim.keymap.set('n', '<leader>**ac', function()
+      vim.cmd 'G commit --amend -m'
+    end, { desc = '[**] Git [A]mend [C]ommit' })
+    vim.keymap.set('n', '<leader>**us', function()
+      vim.cmd 'G restore --staged'
+    end, { desc = '[**] Git [U]nstage [S]taged' })
+    vim.keymap.set('n', '<leader>**pc', function()
+      vim.cmd 'G push origin HEAD'
+    end, { desc = '[**] Git [P]ush [C]urrent branch' })
+    vim.keymap.set('n', '<leader>**re', function()
+      if vim.fn.system 'git rev-parse --verify --quiet main' ~= '' then
+        vim.cmd 'G diff main...HEAD'
+      elseif vim.fn.system 'git rev-parse --verify --quiet master' ~= '' then
+        vim.cmd 'G diff master...HEAD'
+      else
+        print 'Error: Main or master branch not found'
+      end
+    end, { desc = '[**] Git [Re]view changes in branch from main/master' })
+    vim.keymap.set('n', '<leader>**rh', function()
+      vim.cmd 'G reset --hard'
+    end, { desc = '[**] Git [R]eset [H]ard' })
+    vim.keymap.set('n', '<leader>**db', function()
+      vim.cmd 'G branch -D'
+    end, { desc = '[**] Git [D]elete [B]ranch' })
+    vim.keymap.set('n', '<leader>**bf', function()
+      vim.cmd "G branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate"
+    end, { desc = '[**] Git [B]ranch with [F]ormatting' })
+    vim.keymap.set('n', '<leader>**rf', function()
+      vim.cmd "G branch -r --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate"
+    end, { desc = '[**] Git [R]emote branch with [F]ormatting' })
+    vim.keymap.set('n', '<leader>**f', function()
+      vim.cmd 'G fetch'
+    end, { desc = '[**] Git [F]etch' })
+    vim.keymap.set('n', '<leader>**po', function()
+      vim.cmd "G push -u 2>&1 | tee >(cat) | grep 'pull/new' | awk '{print $2}' | xargs open"
+    end, { desc = '[**] Git [P]ush and open pull request' })
+    vim.keymap.set('n', '<leader>**cb', function()
+      vim.cmd "G branch | grep * | cut -d ' ' -f2"
+    end, { desc = '[**] Git [C]urrent [B]ranch name' })
+    vim.keymap.set('n', '<leader>**l1', function()
+      vim.cmd "G log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
+    end, { desc = '[**] Git [L]og format 1' })
+    vim.keymap.set('n', '<leader>**l2', function()
+      vim.cmd "G log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
+    end, { desc = '[**] Git [L]og format 2' })
+    vim.keymap.set('n', '<leader>**l3', function()
+      vim.cmd "G log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n          %C(white)%s%C(reset)%n          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'"
+    end, { desc = '[**] Git [L]og format 3' })
+
+    -- End of custom commands
   end,
 }
