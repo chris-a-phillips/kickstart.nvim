afdc624e3af6f1dfef0d973179318ac6c3ddeba0
Chris Phillips <phillipsachris@gmail.com>
2024-09-17 10:38:44 -0700

    Fix which-key for checkhealth

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,30 +5,30 @@ return {
   config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup()
 
-    -- Document existing key chains using the new which-key spec
-    require('which-key').register {
-      { '<leader>c', group = '[C]ode' },
-      { '<leader>c_', hidden = true }, -- Ignore this key
-      { '<leader>d', group = '[D]ocument' },
-      { '<leader>d_', hidden = true }, -- Ignore this key
-      { '<leader>r', group = '[R]ename' },
-      { '<leader>r_', hidden = true }, -- Ignore this key
-      { '<leader>s', group = '[S]earch' },
-      { '<leader>s_', hidden = true }, -- Ignore this key
-      { '<leader>w', group = '[W]orkspace' },
-      { '<leader>w_', hidden = true }, -- Ignore this key
+    -- Document existing key chains using the new spec
+    local wk = require 'which-key'
+    wk.add {
+      { "<leader>'", group = "[']Arrow File', _ = 'which_key_ignore" },
+      { '<leader>*', group = '[*] Git' },
+      { '<leader>*_', hidden = true },
       { '<leader>;', group = '[;]Arrow Buffer' },
-      { '<leader>;_', hidden = true }, -- Ignore this key
-      { "<leader>'", group = "[']Arrow File" }, -- Fixed typo here
-      { "<leader>'_", hidden = true }, -- Ignore this key
-      { '<leader>u', group = '[U]ndo Tree' },
-      { '<leader>u_', hidden = true }, -- Ignore this key
+      { '<leader>;_', hidden = true },
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
       { '<leader>e', group = '[E]File Explorer' },
-      { '<leader>e_', hidden = true }, -- Ignore this key
-      { '<leader>g', group = '[G] Git' },
-      { '<leader>g_', hidden = true }, -- Ignore this key
+      { '<leader>e_', hidden = true },
       { '<leader>p', group = '[P]review' },
-      { '<leader>p_', hidden = true }, -- Ignore this key
+      { '<leader>p_', hidden = true },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>u', group = '[U]ndo Tree' },
+      { '<leader>u_', hidden = true },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
     }
   end,
 }
